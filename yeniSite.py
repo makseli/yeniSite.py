57e2cd672301a7e4bb09e05c58e3468497e37c20
makseli <verilojistik@gmail.com>
2012-12-15 21:55:36 +0200

    giriş bilgileri değiştirildi

--- yeniSite.py
+++ yeniSite.py
@@ -1,12 +1,13 @@
 ServerName = raw_input("\n ** ServerName giriniz: ")
 print(" \n ServerName \"" + ServerName + "\" olarak atandi !\n")
 
+DocumentRoot = raw_input(" ** DocumentRoot giriniz (ontanimli /var/www/" + ServerName + ") : ")
+if DocumentRoot == "": DocumentRoot = "/var/www/" + ServerName
+print("DocumentRoot \"" + DocumentRoot + "\" olarak atandi !\n")
+
 ServerAdmin =  raw_input(" ServerAdmin E-Posta giriniz (ontanimli= bilgi@" + ServerName + ") : ")
 if ServerAdmin == "": ServerAdmin = "bilgi@" + ServerName
 print(" \n ServerAdmin \"" + ServerAdmin + "\" olarak atandi !\n ")
-
-DocumentRoot = raw_input(" ** DocumentRoot giriniz: ")
-print("DocumentRoot \"" + DocumentRoot + "\" olarak atandi !\n")
 
 ErrorLog = raw_input(" ** ErrorLog giriniz (ontanimli= /var/log/"+ServerName+"): ")
 if ErrorLog == "": ErrorLog = "/var/log/"+ServerName
@@ -33,7 +34,7 @@ VHostDosya.close()
 print("yeni \"" + dosyaAdi + "\" dosya olusturuldu !\n")
 
 LokalHostDosya = open("/etc/hosts", "a")
-LokalHostDosya.write("127.0.0.1" + ServerName)
+LokalHostDosya.write("127.0.0.1		" + ServerName)
 LokalHostDosya.close()
 
 os.system("sudo /etc/init.d/apache2 restart")
